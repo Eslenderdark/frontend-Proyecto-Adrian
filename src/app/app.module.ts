6cffbd978d647efc04b48f5b3067f8bad48e1355
DESKTOP-A3RVKTM\Administrator <eslenderdark@gmail.com>
2024-02-12 13:24:49 +0100

    Aplicacion subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { AuthModule } from '@auth0/auth0-angular';
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +19,12 @@ import { AppRoutingModule } from './app-routing.module';
     authorizationParams: {
       redirect_uri: window.location.origin
     }
-  }),],
+  }), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+}),],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/catalogo/catalogo.page.ts
+++ src/app/catalogo/catalogo.page.ts
@@ -14,6 +14,7 @@ import { environment } from '../../environments/environment';
 export class CatalogoPage implements OnInit {
 
   constructor(public auth: AuthService, private http: HttpClient, private alertController: AlertController, private router: Router) { }
+
   public cortes: any;
   public user: any;
   public datos_user: any;
@@ -151,7 +152,7 @@ export class CatalogoPage implements OnInit {
   async borrar_corte(name: any) {
     console.log("Corte")
     console.log(name)
-    this.http.delete(environment.backend_host + 'cortes/' + name).subscribe(
+    this.http.delete(environment.backend_host + '/cortes/' + name).subscribe(
       (response: any) => {
         console.log('Respuesta del backend eliminar', response);
         window.location.reload();
